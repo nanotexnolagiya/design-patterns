bc82858b4417cf3efd3499b153fb61831ec32da9
nanotexnolagiya <nanotexnolagiya@gmail.com>
2020-02-16 23:36:08 +0500

    Fix abstract factory

--- src/creational/abstract-factory/AbstractFactory.ts
+++ src/creational/abstract-factory/AbstractFactory.ts
@@ -1,5 +1,3 @@
-// import CarFactory from './CarFactory'
-// import LaptopFactory from './LaptopFactory'
 import { ICar } from './cars/Car'
 import { ILaptop } from './laptops/Laptop'
 
@@ -7,17 +5,3 @@ export interface IAbstractFactory {
   createCar(): ICar
   createLaptop(): ILaptop
 }
-
-// export default class AbstractFactory {
-//   private static readonly CAR_FACTORY = 'car'
-//   private static readonly LAPTOP_FACTORY = 'laptop'
-//   createFactory(factoryName: string) {
-//     if (factoryName === AbstractFactory.CAR_FACTORY) {
-//       return new CarFactory()
-//     } else if (factoryName === AbstractFactory.LAPTOP_FACTORY) {
-//       return new LaptopFactory()
-//     } else {
-//       throw new Error('Not found this factory')
-//     }
-//   }
-// }
